e8a0155a7ed90a6d92e0742485be3f1abd9841aa
Nam02-java <thanhnamtruong22102002@gmail.com>
2025-03-27 11:26:01 +0700

    update for ubuntu

--- src/main/java/com/example/speech/aiservice/vn/service/video/VideoCreationService.java
+++ src/main/java/com/example/speech/aiservice/vn/service/video/VideoCreationService.java
@@ -47,7 +47,7 @@ public class VideoCreationService {
             return new CreateVideoResponseDTO("⚠️ No video files found!", null, null, null);
         }
 
-        // Create a directory for the exploit if it does not exist.
+        // Create a directory for the exploit if it does not exist
         String safeNovelTitle = fileNameService.sanitizeFileName(novel.getTitle());
         String novelDirectory = uploadDirectoryPath + File.separator + safeNovelTitle;
         fileNameService.ensureDirectoryExists(novelDirectory);
@@ -88,77 +88,73 @@ public class VideoCreationService {
 
     /**
      * for ubuntu desktop
+     *
      * @param audioPath
      * @param imagePath
      * @param novel
      * @param chapter
      * @return
      */
-//    public CreateVideoResponseDTO createVideoResponseDTO(String audioPath, String imagePath, Novel novel, Chapter chapter) {
-//
-//        String ffmpegPath = propertiesService.getFfmpegPath();
-//        String uploadDirectoryPath = propertiesService.getUploadDirectory();
-//        String fileExtension = propertiesService.getUploadFileExtension();
-//
-//        if (audioPath == null) {
-//            System.out.println("⚠️ No video files found!");
-//            return new CreateVideoResponseDTO("⚠️ No video files found!", null, null, null);
-//        }
-//
-//
-//        // Tạo thư mục cho bộ truyện nếu chưa tồn tại
-//        String safeNovelTitle = fileNameService.sanitizeFileName(novel.getTitle());
-//        String novelDirectory = uploadDirectoryPath + File.separator + safeNovelTitle;
-//        fileNameService.ensureDirectoryExists(novelDirectory);
-//
-//        // Xử lý tên file chương hợp lệ
-//        String safeChapterTitle = fileNameService.sanitizeFileName(chapter.getTitle()) + fileExtension;
-//        String audioFilePath = novelDirectory + File.separator + safeChapterTitle;
-//
-//
-//        String videoFilePath = fileNameService.getAvailableFileName(audioFilePath, chapter.getTitle(), fileExtension);
-//
-//        // FFmpeg command
-////        String command = "\"" + ffmpegPath + "\" -loop 1 -i \"" + imagePath + "\" -i \"" + audioPath +
-////                "\" -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest \"" + videoFilePath + "\"";
-////
-//
-//
-//        List<String> command = Arrays.asList(
-//                ffmpegPath,
-//                "-loop", "1",
-//                "-i", imagePath,
-//                "-i", audioPath,
-//                "-c:v", "libx264",
-//                "-tune", "stillimage",
-//                "-c:a", "aac",
-//                "-b:a", "192k",
-//                "-pix_fmt", "yuv420p",
-//                "-shortest",
-//                videoFilePath
-//        );
-//
-//        System.out.println("Executing command: " + command);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        System.out.println("Run command: " + command);
-//        try {
-//            ProcessBuilder pb = new ProcessBuilder(command);
-//            pb.redirectErrorStream(true);
-//            Process process = pb.start();
-//
-//            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-//            String line;
-//            while ((line = reader.readLine()) != null) {
-//                System.out.println(line);
-//            }
-//
-//            int exitCode = process.waitFor();
-//            if (exitCode == 0) {
-//                System.out.println("✅ Complete! Output file: " + videoFilePath);
-//            } else {
-//                System.out.println("⚠️ FFmpeg encountered an error.");
-//            }
-//        } catch (IOException | InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        return new CreateVideoResponseDTO("Create a successful video", imagePath, audioPath, videoFilePath);
-//    }
+    public CreateVideoResponseDTO createVideoResponseDTsO(String audioPath, String imagePath, Novel novel, Chapter chapter) {
+
+        String ffmpegPath = propertiesService.getFfmpegPath();
+        String uploadDirectoryPath = propertiesService.getUploadDirectory();
+        String uploadFileExtension = propertiesService.getUploadFileExtension();
+
+        if (audioPath == null) {
+            System.out.println("⚠️ No video files found!");
+            return new CreateVideoResponseDTO("⚠️ No video files found!", null, null, null);
+        }
+
+
+        // Create a directory for the exploit if it does not exist
+        String safeNovelTitle = fileNameService.sanitizeFileName(novel.getTitle());
+        String novelDirectory = uploadDirectoryPath + File.separator + safeNovelTitle;
+        fileNameService.ensureDirectoryExists(novelDirectory);
+
+
+        // Handling valid chapter file names
+        String safeChapterTitle = fileNameService.sanitizeFileName(chapter.getTitle());
+        //String audioFilePath = novelDirectory + File.separator + safeChapterTitle;
+        String videoFilePath = fileNameService.getAvailableFileName(novelDirectory, safeChapterTitle, uploadFileExtension);
+
+        // FFmpeg command
+        List<String> command = Arrays.asList(
+                ffmpegPath,
+                "-loop", "1",
+                "-i", imagePath,
+                "-i", audioPath,
+                "-c:v", "libx264",
+                "-tune", "stillimage",
+                "-c:a", "aac",
+                "-b:a", "192k",
+                "-pix_fmt", "yuv420p",
+                "-shortest",
+                videoFilePath
+        );
+
+        System.out.println("Executing command: " + command);
+        System.out.println("Run command: " + command);
+        try {
+            ProcessBuilder pb = new ProcessBuilder(command);
+            pb.redirectErrorStream(true);
+            Process process = pb.start();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            int exitCode = process.waitFor();
+            if (exitCode == 0) {
+                System.out.println("✅ Complete! Output file: " + videoFilePath);
+            } else {
+                System.out.println("⚠️ FFmpeg encountered an error.");
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return new CreateVideoResponseDTO("Create a successful video", imagePath, audioPath, videoFilePath);
+    }
 }
